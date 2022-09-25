13b604ac47a56a96bcb1faba44ca400d50591c84
Casanova Geary <CasanovaGeary@Gmail.com>
2022-09-25 18:51:58 -0500

    Initial: Copy/Paste from version 1.

---
+++ main.js
@@ -0,0 +1,47 @@
+"use strict";
+
+// FETCH GLITCH API
+function fetchMovies () {
+    fetch('https://aluminum-coral-comic.glitch.me/movies')
+        .then(response => {
+            console.log(response);
+            if(!response.ok) {
+                throw Error('ERROR');
+            }
+            return response.json();
+        })
+        .then(movieData => {
+            console.log(movieData);
+            $('#app').innerHTML = '<h1>Hurry Up, Cas!</h1>';
+        }).catch(error => {
+        console.log(error);
+    });
+}
+fetchMovies();
+
+// LOADER
+document.onreadystatechange = function () {
+    if (document.readyState !== "complete") {
+        document.querySelector("body").style.visibility = "hidden";
+        document.querySelector("#loader").style.visibility = "visible";
+    } else {
+        setTimeout(() => {
+            document.querySelector("#loader").style.display = "none";
+            document.querySelector("body").style.visibility = "visible";
+        }, 1000);
+
+    }
+};
+
+$('#find-movie').on('click', function () {
+    alert("Search button works.");
+});
+
+
+
+
+
+
+
+
+
