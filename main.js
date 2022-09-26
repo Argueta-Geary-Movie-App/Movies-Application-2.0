a8222de55ce6a6fda730a3a64c298cfd718427cb
Casanova Geary <CasanovaGeary@Gmail.com>
2022-09-25 21:49:15 -0500

    re-accomplished main.js. Error retrieving data into insertAdjacentHTML. Will refactor inside a card on HTML for a functional app for now.

--- main.js
+++ main.js
@@ -1,48 +1,56 @@
 "use strict";
 
 /* FETCH GLITCH API */
-function fetchMovies () {
+function fetchMovies() {
     fetch('https://aluminum-coral-comic.glitch.me/movies')
         .then(response => {
             console.log(response);
-            if(!response.ok) {
+            if (!response.ok) {
                 throw Error('ERROR');
             }
             return response.json();
         })
         .then(movieData => {
             console.log(movieData);
-            $('#app').insertAdjacentHTML('afterbegin', '<h1>Hurry Up, Cas!</h1>');
-        }).catch(error => {
-        console.log(error);
-    });
+            const html = movieData(movie => {
+                return `
+                <div id="card">
+                    <div class="movie">
+                        <p class="title">Title: ${movie.title}</p>
+                        <p class="director">Director: ${movie.director}</p>
+                        <p class="rating">Rating: ${movie.rating}</p>
+                        <p class="genre">Genre: ${movie.genre}</p>
+                        <p class="plot">Plot: ${movie.plot}</p>
+                    </div>
+                </div>
+                `;
+             })
+             .join("");
+            document.querySelector('#app').insertAdjacentHTML('afterbegin', html);
+        })
+        .catch(error => {
+            console.log(error);
+        });
 }
 fetchMovies();
 
-/* LOADER */
-document.onreadystatechange = function () {
-    if (document.readyState !== "complete") {
-        document.querySelector("body").style.visibility = "hidden";
-        document.querySelector("#loader").style.visibility = "visible";
-    } else {
-        setTimeout(() => {
-            document.querySelector("#loader").style.display = "none";
-            document.querySelector("body").style.visibility = "visible";
-        }, 1000);
-
-    }
-};
-
-/* EVENT LISTENERS */
-$('#find-movie').on('click', function () {
-    alert("Search button works.");
-});
 
 
+                /* LOADER */
+                document.onreadystatechange = function () {
+                    if (document.readyState !== "complete") {
+                        document.querySelector("body").style.visibility = "hidden";
+                        document.querySelector("#loader").style.visibility = "visible";
+                    } else {
+                        setTimeout(() => {
+                            document.querySelector("#loader").style.display = "none";
+                            document.querySelector("body").style.visibility = "visible";
+                        }, 1000);
 
+                    }
+                };
 
-
-
-
-
-
+                /* EVENT LISTENERS */
+                $('#find-movie').on('click', function () {
+                    alert("Search button works.");
+                });
