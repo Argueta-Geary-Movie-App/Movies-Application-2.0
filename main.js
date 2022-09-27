f63e8b957783ea656fd7b89beec5cbdaeaeffb2a
3arglop <estefania.arguetalopez13@gmail.com>
2022-09-27 13:23:12 -0500

    Hello

--- main.js
+++ main.js
@@ -1,56 +1,50 @@
 "use strict";
 
 /* FETCH GLITCH API */
-function fetchMovies() {
+function fetchMovies () {
     fetch('https://aluminum-coral-comic.glitch.me/movies')
         .then(response => {
             console.log(response);
-            // const data = response.json();
-            if (!response.ok) {
+            if(!response.ok) {
                 throw Error('ERROR');
             }
             return response.json();
         })
-        .then(data => {
-            console.log(data);
-            const html = data.map(movie => {
-                return `
-                <div id="card">
-                    <div class="movie">
-                        <p class="title">Title: ${movie.title}</p>
-                        <p class="director">Director: ${movie.director}</p>
-                        <p class="rating">Rating: ${movie.rating}</p>
-                        <p class="genre">Genre: ${movie.genre}</p>
-                        <p class="plot">Plot: ${movie.plot}</p>
-                    </div>
-                </div>
-                `;
-             })
-             .join("");
-            console.log(html);
-            $('#app').html(html);
-        });
+        .then(movieData => {
+            console.log(movieData);
+            $('#app').insertAdjacentHTML('afterbegin', '<h1>Hurry Up, Cas!</h1>');
+        }).catch(error => {
+        console.log(error);
+    });
 }
-
 fetchMovies();
 
+//hello
+
+/* LOADER */
+document.onreadystatechange = function () {
+    if (document.readyState !== "complete") {
+        document.querySelector("body").style.visibility = "hidden";
+        document.querySelector("#loader").style.visibility = "visible";
+    } else {
+        setTimeout(() => {
+            document.querySelector("#loader").style.display = "none";
+            document.querySelector("body").style.visibility = "visible";
+        }, 1000);
+
+    }
+};
+
+/* EVENT LISTENERS */
+$('#find-movie').on('click', function () {
+    alert("Search button works.");
+});
 
 
-                /* LOADER */
-                document.onreadystatechange = function () {
-                    if (document.readyState !== "complete") {
-                        document.querySelector("body").style.visibility = "hidden";
-                        document.querySelector("#loader").style.visibility = "visible";
-                    } else {
-                        setTimeout(() => {
-                            document.querySelector("#loader").style.display = "none";
-                            document.querySelector("body").style.visibility = "visible";
-                        }, 1000);
 
-                    }
-                };
 
-                /* EVENT LISTENERS */
-                $('#find-movie').on('click', function () {
-                    alert("Search button works.");
-                });
+
+
+
+
+
